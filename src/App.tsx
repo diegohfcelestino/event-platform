9cbf2512276cbbf6fa90692e311d61ebb12ff32b
diegohfcelestino <diegohfcelestino@gmail.com>
2022-06-21 19:30:42 -0300

    Criando o Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,10 @@
-import { gql, useQuery } from '@apollo/client'
+import { Event } from './pages/Event'
 
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`
-
-interface Lesson {
-  id: string
-  title: string
-}
 function App() {
-  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY)
-
-  console.log('api', data)
-
   return (
-    <ul>
-      {data?.lessons.map(lesson => {
-        return <li key={lesson.id}>{lesson.title}</li>
-      })}
-    </ul>
+    <div>
+      <Event />
+    </div>
   )
 }
 
