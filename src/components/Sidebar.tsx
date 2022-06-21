dabf54318bfe4343e50152618f1c2794270480ef
diegohfcelestino <diegohfcelestino@gmail.com>
2022-06-21 19:47:16 -0300

    Criando o componente Sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,3 +1,9 @@
 export function Sidebar() {
-  return <h1>Sidebar</h1>
+  return (
+    <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
+      <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
+        Cronograma de Aulas
+      </span>
+    </aside>
+  )
 }
--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,3 +1,3 @@
 export function Video() {
-  return <h1>Video</h1>
+  return <div className="flex-1">Video</div>
 }
