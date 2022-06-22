977912f9c49ab870cd977e4a24412f4efd12271f
diegohfcelestino <diegohfcelestino@gmail.com>
2022-06-22 20:19:37 -0300

    Finalizando a terceira aula do ignite Lab, finalizando o componente de vídeo e lesson

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,29 +1,30 @@
-import { CheckCircle, Lock } from 'phosphor-react'
-import { isPast, format } from 'date-fns'
-import ptBR from 'date-fns/locale/pt-BR'
+import { CheckCircle, Lock } from "phosphor-react";
+import { format, isPast } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
+import { Link } from "react-router-dom";
 
 interface LessonProps {
-  title: string
-  slug: string
-  availableAt: Date
-  type: 'live' | 'class'
+  title: string;
+  slug: string;
+  availableAt: Date;
+  type: "live" | "class";
 }
 
 export function Lesson(props: LessonProps) {
-  const isLessonAvailable = isPast(props.availableAt)
+  const isLessonAvailable = isPast(props.availableAt);
   const availableDateFormatted = format(
     props.availableAt,
-    "EEEE' • 'd' de 'MMMM' • 'k'h'mm'",
+    "EEEE' • 'd' de 'MMMM' • 'k'h'mm",
     {
       locale: ptBR
     }
-  )
+  );
 
   return (
-    <a href="#">
+    <Link to={`/event/lesson/${props.slug}`} className="group">
       <span className="text-gray-300">{availableDateFormatted}</span>
 
-      <div className="rounded border border-gray-500 p-4 mt-2">
+      <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
         <header className="flex items-center justify-between">
           {isLessonAvailable ? (
             <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
@@ -33,16 +34,17 @@ export function Lesson(props: LessonProps) {
           ) : (
             <span className="text-sm text-orange-500 font-medium flex items-center gap-2">
               <Lock size={20} />
-              Em Breve
+              Em breve
             </span>
           )}
+
           <span className="text-xs rounded py-[0.125rem] px-2 text-white border border-green-300 font-bold">
-            {props.type === 'live' ? 'AO VIVO' : 'AULA PRÁTICA'}
+            {props.type === "live" ? "AO VIVO" : "AULA PRÁTICA"}
           </span>
         </header>
 
         <strong className="text-gray-200 mt-5 block">{props.title}</strong>
       </div>
-    </a>
-  )
+    </Link>
+  );
 }
